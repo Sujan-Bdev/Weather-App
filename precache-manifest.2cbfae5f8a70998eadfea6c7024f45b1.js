2e25985477c26aae857c87202fa52d0919878f1c
“Sujan” <sujanbudhathoki123@gmail.com>
2020-05-23 22:15:03 +0545

    Updates

---
+++ precache-manifest.2cbfae5f8a70998eadfea6c7024f45b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40e8c33cfbd83e0b26c84a50dd816a86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74e19709bdb59b65d3d3",
+    "url": "/static/css/main.2ed76e0a.chunk.css"
+  },
+  {
+    "revision": "b614e63e89683407f57e",
+    "url": "/static/js/2.62301120.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.62301120.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74e19709bdb59b65d3d3",
+    "url": "/static/js/main.63a0f8e4.chunk.js"
+  },
+  {
+    "revision": "9f1e78d60d8b52bd3d90",
+    "url": "/static/js/runtime-main.a86148e1.js"
+  }
+]);
